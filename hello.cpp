bc0505f174e8337d6cd6ad1f3c1acef44827b185
bogdanraonic <bogdan.raonic@polytechnique.edu>
2019-10-03 09:35:56 +0200

    Added Hello.cpp program

---
+++ hello.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    std::string s;
+    std::cin >> s;
+    if (s.length() == n)
+    {
+        bool b = true;
+	for(int i=0;i<n/2;i++)
+	    {
+		b = b && (s[i]==s[n-1-i]);
+            }
+	if (b)
+	{
+	    std::cout << "True" << std::endl;
+	}
+	else
+	{
+	    std::cout << "False" << std::endl;	
+	}
+        return b;
+    }
+    else
+    {	
+	std::cout << "False" << std::endl;
+        return false;
+    }
+	
+    return 0;
+}
+
